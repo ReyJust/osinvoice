aab4bcb70ff909f03ab3f6ff9e9f5d3af050d3d3
ReyJust <jusmayer@outlook.com>
2026-05-24 09:11:29 +1000

    feat: conditional navbar display based on auth status

--- components/navigation/nav-bar.tsx
+++ components/navigation/nav-bar.tsx
@@ -6,27 +6,43 @@ import { Button } from "@/components/ui/button"
 import {
     NavMenu,
 } from "@/components/navigation/nav-menu"
-import { CompanySwitcher } from "@/components/navigation/company-switcher";
+import { UserMenu } from "@/components/navigation/user-menu";
+import Link from "next/link";
+
+import { Tooltip, TooltipContent, TooltipTrigger } from "../ui/tooltip";
+import { cookies } from 'next/headers'
+import { createClient } from '@/utils/supabase/server'
+
 // Types
-import { type company } from "@/lib/types/navigation";
+// import { type User } from "@/lib/types/user";
 
+export async function NavBar() {
 
-export function NavBar({
-    companies: companies,
-    current_company: current_company,
-}: {
-    companies: company[],
-    current_company: company
-}) {
+    const cookieStore = await cookies()
+    const client = createClient(cookieStore)
+    const { data: { user } } = await client.auth.getUser()
+
     return (
         <header className="flex h-16 shrink-0 justify-between items-center gap-2 border-b px-4">
-            <div>
-                <CompanySwitcher companies={companies} current_company={current_company} />
+            <div >
+                {user ? <UserMenu user={user} /> : <Tooltip>
+                    <TooltipTrigger>
+                        {/* <Button size="sm" onClick={() => router.push('/signup')}>
+                            Sign Up / Login
+                        </Button> */}
+                        <Link href="/login" className="text-sm font-medium">
+                            Sign Up / Login
+                        </Link>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        <p>By signing up, you will be able to save invoices and all informations for fast usage.</p>
+                    </TooltipContent>
+                </Tooltip>}
             </div>
-            <NavMenu />
+            {user ? <NavMenu /> : null}
 
             <Button size="sm">
-                New
+                New Invoice
                 <HugeiconsIcon
                     icon={PlusSignIcon}
                 // size={24}
