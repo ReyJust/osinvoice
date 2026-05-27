8d87993b088c0df26ef0b1ba38eb7db331e2c600
ReyJust <jusmayer@outlook.com>
2026-05-28 09:07:10 +1000

    feat: invoice list page

--- app/invoice/page.tsx
+++ app/invoice/page.tsx
@@ -1,62 +1,35 @@
-import { createClient } from '@/utils/supabase/server'
-import { cookies } from 'next/headers'
+import { createClient } from "@/utils/supabase/server"
+import { cookies } from "next/headers"
 
-import { Invoice } from '@/lib/types/invoice'
-import { InvoiceCard } from '@/components/invoice/invoice-card'
+import { Invoice } from "@/lib/types/invoice"
+import { InvoiceCard } from "@/components/invoice/invoice-card"
 
-const invoices: Invoice[] = [{
-  id: '1',
-  company_id: '1',
-  client_id: '1',
-  content: [],
-},
-{
-  id: '2',
-  company_id: '1',
-  client_id: '1',
-  content: []
-},
-{
-  id: '3',
-  company_id: '1',
-  client_id: '1',
-  content: []
-},
-{
-  id: '4',
-  company_id: '1',
-  client_id: '1',
-  content: []
-},
-{
-  id: '5',
-  company_id: '1',
-  client_id: '1',
-  content: []
-},
-{
-  id: '6',
-  company_id: '1',
-  client_id: '1',
-  content: []
-}]
+const invoices: Invoice[] = [
+  {
+    id: 1,
+    description: "asdfasdf",
+    company_id: 1,
+    client_id: 1,
+    created_at: new Date(),
+    lines: [],
+    notes: "",
+    status: "draft",
+  },
+]
 
 export default async function Page() {
   const cookieStore = await cookies()
   const supabase = createClient(cookieStore)
 
-  const { data: todos } = await supabase.from('todos').select()
-  console.log('todos', todos)
+  // const { data: todos } = await supabase.from("todos").select()
+  // console.log("todos", todos)
 
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
       <div className="grid auto-rows-min gap-4 md:grid-cols-5">
         {invoices.map((invoice) => {
-          return (
-            <InvoiceCard key={invoice.id} invoice={invoice} />
-          )
-        })
-        }
+          return <InvoiceCard key={invoice.id} invoice={invoice} />
+        })}
       </div>
     </div>
   )
--- components/invoice/invoice-card.tsx
+++ components/invoice/invoice-card.tsx
@@ -1,31 +1,49 @@
 import { Invoice } from "@/lib/types/invoice"
+
 import {
-    Card,
-    CardAction,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card"
 
-export function InvoiceCard({ invoice
-}: {
-    invoice: Invoice
-}) {
-    return (
-        <Card id={invoice.id}>
-            <CardHeader>
-                {/* <CardTitle>{invoice.title}</CardTitle> */}
-                {/* <CardDescription>{invoice.description}</CardDescription> */}
-                {/* <CardAction>{invoice.action}</CardAction> */}
-            </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
-            </CardFooter>
-        </Card>
-    )
+import { Button } from "../ui/button"
+import { HugeiconsIcon } from "@hugeicons/react"
+
+import { DeleteInvoiceForm } from "./delete-invoice-form"
+import { formatDate } from "@/lib/formatters"
+import { Edit03Icon } from "@hugeicons/core-free-icons"
+
+export function InvoiceCard({ invoice }: { invoice: Invoice }) {
+  return (
+    <Card id={"invoice-" + invoice.id.toString()}>
+      <CardHeader>
+        <CardTitle>#{invoice.id}</CardTitle>
+        <CardDescription>
+          {`${invoice.description} ${invoice.company_id}`}
+          {/* TODO: Use company name */}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{invoice.status}</p> {/* TODO: Colorize status */}
+        <p className="text-xs text-gray-400">
+          Created on: {formatDate(invoice.created_at)}
+        </p>
+      </CardContent>
+      <CardFooter className="justify-end">
+        <Button variant="default" size="icon">
+          <HugeiconsIcon
+            icon={Edit03Icon}
+            // className="text-muted-foreground"
+            // size={24}
+            // color="#FFFFFF"
+            // strokeWidth={1.5}
+          />
+        </Button>
+        <DeleteInvoiceForm invoice={invoice} />
+      </CardFooter>
+    </Card>
+  )
 }
